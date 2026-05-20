9b28b212246bab7c277cee1734b3ec17e904868d
ajayanilsree <ajayanilsree@gmail.com>
2026-05-20 08:15:28 +0530

    Remove local media files after Cloudinary setup

--- whn-bookings/whn_bookings_dj/settings.py
+++ whn-bookings/whn_bookings_dj/settings.py
@@ -130,6 +130,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
